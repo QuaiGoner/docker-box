814915bcfb4a9b034567109f04ea3b5e115970ee
QuaiGoner <41058180+QuaiGoner@users.noreply.github.com>
2023-01-06 20:31:41 +0700

    Try Closing retroarch from Moonlight
    and move retroarch config to script

--- overlay/etc/cont-init.d/90-configure_retroarch.sh
+++ overlay/etc/cont-init.d/90-configure_retroarch.sh
@@ -7,4 +7,6 @@ chmod +x /usr/bin/start-retroarch.sh
 echo "Ensure proper Retroarch folder permissions"
 chmod -R a+rw /home/${USER}/.config/retroarch/
 chown -R ${PUID}:${PGID} /home/${USER}/.config/retroarch/
+echo "Copy Retroarch config from template"
+cp -vf /templates/retroarch/* /home/default/.config/retroarch/
 echo "DONE"
