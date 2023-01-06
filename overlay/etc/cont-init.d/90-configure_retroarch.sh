5c7467f70be5d10bc520ad1d533762f6f3e4d8e4
QuaiGoner <41058180+QuaiGoner@users.noreply.github.com>
2023-01-06 13:48:43 +0700

    Update 90-configure_retroarch.sh

--- overlay/etc/cont-init.d/90-configure_retroarch.sh
+++ overlay/etc/cont-init.d/90-configure_retroarch.sh
@@ -4,4 +4,6 @@ echo "***Configture Retroarch****"
 echo "Enable Retroarch Autostart"
 sed -i 's|^autostart.*=.*$|autostart=true|' /etc/supervisor.d/retroarch.ini
 chmod +x /usr/bin/start-retroarch.sh
+chmod -R a+rw /home/${USER}/.config/retroarch/
+chown -R ${PUID}:${PGID} /home/${USER}/.config/retroarch/
 echo "DONE"
