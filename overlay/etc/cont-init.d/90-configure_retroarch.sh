55186d52e80bd1ca38d20f521dd198f0db9b5643
QuaiGoner <41058180+QuaiGoner@users.noreply.github.com>
2023-01-06 20:41:34 +0700

    Update 90-configure_retroarch.sh

--- overlay/etc/cont-init.d/90-configure_retroarch.sh
+++ overlay/etc/cont-init.d/90-configure_retroarch.sh
@@ -3,7 +3,7 @@ set -e
 echo "***Configture Retroarch****"
 #echo "Disable Retroarch Autostart"
 #sed -i 's|^autostart.*=.*$|autostart=false|' /etc/supervisor.d/retroarch.ini
-chmod +x /usr/bin/start-retroarch.sh
+#chmod +x /usr/bin/start-retroarch.sh
 echo "Ensure proper Retroarch folder permissions"
 chmod -R a+rw /home/${USER}/.config/retroarch/
 chown -R ${PUID}:${PGID} /home/${USER}/.config/retroarch/
