4347e5e94dba8202274b2f015c2a7c46dc863da5
QuaiGoner <41058180+QuaiGoner@users.noreply.github.com>
2023-03-18 12:16:56 +0700

    Update 90-configure_retroarch.sh

--- overlay/etc/cont-init.d/90-configure_retroarch.sh
+++ overlay/etc/cont-init.d/90-configure_retroarch.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 set -e
-echo "***Configture Retroarch****"
+echo "***Configure Retroarch****"
 #echo "Disable Retroarch Autostart"
 #sed -i 's|^autostart.*=.*$|autostart=false|' /etc/supervisor.d/retroarch.ini
 #chmod +x /usr/bin/start-retroarch.sh
@@ -8,14 +8,14 @@ echo "Ensure proper Retroarch folder permissions"
 #chmod -R a+rw /home/${USER}/.config/retroarch/
 #chown -R ${PUID}:${PGID} /home/${USER}/.config/retroarch/
 echo "Copy Retroarch config from template"
-cp -vf /templates/retroarch/* /home/default/.config/retroarch/
-# Copy pre-installed cores from the retroarch ppa
-cp -u /usr/lib/$(uname -m)-linux-gnu/libretro/* "/home/default/.config/retroarch/cores/"
+# cp -vf /templates/retroarch/* /home/default/.config/retroarch/
+# # Copy pre-installed cores from the retroarch ppa
+# cp -u /usr/lib/$(uname -m)-linux-gnu/libretro/* "/home/default/.config/retroarch/cores/"
 
-# if there are no assets, manually download them
-if [ ! -d "$CFG_DIR/assets" ]; then
-    wget -q --show-progress -P /tmp https://buildbot.libretro.com/assets/frontend/assets.zip
-    7z x /tmp/assets.zip -bso0 -bse0 -bsp1 -o"/home/default/.config/retroarch/assets"
-    rm /tmp/assets.zip
-fi
+# # if there are no assets, manually download them
+# if [ ! -d "$CFG_DIR/assets" ]; then
+    # wget -q --show-progress -P /tmp https://buildbot.libretro.com/assets/frontend/assets.zip
+    # 7z x /tmp/assets.zip -bso0 -bse0 -bsp1 -o"/home/default/.config/retroarch/assets"
+    # rm /tmp/assets.zip
+# fi
 echo "DONE"
